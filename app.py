c7a2a025a6bfb9cc9a0166e3925620d8a424e42b
Juan Esteban Victoria <juane.victorial@uqvirtual.edu.co>
2025-05-25 14:22:21 -0500

    Conexión Flask-SQL Server funcionando y base ProyectoBD creada

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
- 
+from flask import Flask, render_template
+from db import get_connection
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    conn = get_connection()
+    if conn:
+        mensaje = "Conexión a la base de datos exitosa 🎉"
+        conn.close()
+    else:
+        mensaje = "Error de conexión 😢"
+    return render_template('index.html', mensaje=mensaje)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
--- db.py
+++ db.py
@@ -1,1 +1,14 @@
- 
+import pyodbc
+
+def get_connection():
+    try:
+        connection = pyodbc.connect(
+            "DRIVER={ODBC Driver 17 for SQL Server};"
+            "SERVER=DESKTOP-87HFKPA;"  # Cambia si tu instancia es diferente
+            "DATABASE=ProyectoBD;"        # Reemplaza con tu base de datos
+            "Trusted_Connection=yes;"        # O usa UID y PWD si no usas autenticación de Windows
+        )
+        return connection
+    except Exception as e:
+        print("Error en la conexión:", e)
+        return None
